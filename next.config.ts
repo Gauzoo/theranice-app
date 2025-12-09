7a3115e146c6a8127122b59fd6d3cc19c884c78f
Gauzoo <gauthier.guerin@gmail.com>
2025-12-09 10:51:07 +0100

    fix security 09/12

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Supprime le warning middleware (temporaire jusqu'à migration)
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
